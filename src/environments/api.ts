2fcb8e120efc12b288db6a486bec56ed4695faa4
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2020-12-13 19:03:20 +0100

    add upload page to quick-start

--- src/environments/api.ts
+++ src/environments/api.ts
@@ -1,2 +1,2 @@
-export const apiUrl = 'http://localhost:5000';
-//export const apiUrl = 'https://budgetapp-api-694202137-dev.herokuapp.com';
+//export const apiUrl = 'http://localhost:5000';
+export const apiUrl = 'https://budgetapp-api-694202137-dev.herokuapp.com';
