9c4eebdc7cd469bae9cda564adedabdb57c152bd
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2020-12-28 17:49:40 +0100

    removed old code for old charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,125 @@ import { combineLatest } from 'rxjs';
 import { FixedPointsService } from './services/budget/fixed-points.service';
 import { MatDrawer, MatSidenav } from '@angular/material/sidenav';
 
+
+import * as Highcharts from 'highcharts';
+
+
+
+const lightChartOptions = {
+
+  "colors": ["#0266C8", "#F90101", "#F2B50F", "#00933B"],
+  "chart": {
+    "style": {
+      "fontFamily": "Roboto",
+      "color": "#444444"
+    }
+  },
+  "xAxis": {
+    "gridLineWidth": 1,
+    "gridLineColor": "#F3F3F3",
+    "lineColor": "#F3F3F3",
+    "minorGridLineColor": "#F3F3F3",
+    "tickColor": "#F3F3F3",
+    "tickWidth": 1
+  },
+  "yAxis": {
+    "gridLineColor": "#F3F3F3",
+    "lineColor": "#F3F3F3",
+    "minorGridLineColor": "#F3F3F3",
+    "tickColor": "#F3F3F3",
+    "tickWidth": 1
+  },
+  "legendBackgroundColor": "rgba(0, 0, 0, 0.5)",
+  "background2": "#505053",
+  "dataLabelsColor": "#B0B0B3",
+  "textColor": "#C0C0C0",
+  "contrastTextColor": "#F0F0F3",
+  "maskColor": "rgba(255,255,255,0.3)"
+
+}
+const darkChartOptions = {
+  "colors": ["#A9CF54", "#C23C2A", "#FFFFFF", "#979797", "#FBB829"],
+  "chart": {
+    "backgroundColor": "#424242",
+    "style": {
+      "color": "white"
+    }
+  },
+  "legend": {
+    "enabled": true,
+    //"align": "right",
+    //"verticalAlign": "bottom",
+    "itemStyle": {
+      "color": "#C0C0C0"
+    },
+    "itemHoverStyle": {
+      "color": "#C0C0C0"
+    },
+    "itemHiddenStyle": {
+      "color": "#444444"
+    }
+  },
+  "title": {
+    "text": '',
+    "style": {
+      "color": "#FFFFFF"
+    }
+  },
+  "tooltip": {
+    "backgroundColor": "#1C242D",
+    "borderColor": "#1C242D",
+    "borderWidth": 1,
+    "borderRadius": 0,
+    "style": {
+      "color": "#FFFFFF"
+    }
+  },
+  "subtitle": {
+    "style": {
+      "color": "#666666"
+    }
+  },
+  "xAxis": {
+    "gridLineColor": "#2E3740",
+    "gridLineWidth": 1,
+    "labels": {
+      "style": {
+        "color": "#a2a2a2"
+      }
+    },
+    "lineColor": "#2E3740",
+    "tickColor": "#2E3740",
+    "title": {
+      "style": {
+        "color": "#FFFFFF"
+      },
+      "text": ''
+    }
+  },
+  "yAxis": {
+    "gridLineColor": "#2E3740",
+    "gridLineWidth": 1,
+    "labels": {
+      "style": {
+        //"color": "#FF0000"
+        "color": "#a2a2a2"
+      },
+    },
+    lineColor: "#2E3740",
+    tickColor: "#2E3740",
+
+    "title": {
+      "style": {
+        "color": "#FFFFFF"
+      },
+      "text": ''
+    }
+
+  }
+
+}
+
 type Theme = 'light-theme' | 'dark-theme';
 
 
@@ -126,6 +245,20 @@ export class AppComponent implements OnInit, AfterViewInit {
 
 
   }
+  initTheme() {
+
+    if (this.isDarkTheme) {
+      this.setCurrentTheme('dark-theme');
+      Highcharts.setOptions(darkChartOptions)
+    } else {
+      this.setCurrentTheme('light-theme');
+      Highcharts.setOptions(lightChartOptions)
+
+
+    }
+
+  }
+
   setCurrentTheme(theme: Theme) {
     this.selectedTheme = theme;
   }
@@ -166,16 +299,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   }
 
-  initTheme() {
 
-    if (this.isDarkTheme) {
-      this.setCurrentTheme('dark-theme');
-    } else {
-      this.setCurrentTheme('light-theme');
-
-    }
-
-  }
 
   redirectToQuickStartIfNewUser() {
 
@@ -205,6 +329,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   changeTheme() {
     this.isDarkTheme = !this.isDarkTheme;
     localStorage.setItem('isDarkTheme', this.isDarkTheme);
+    this.initTheme();
   }
 
   activateHandsetLayout() {
--- src/app/components/charts/predictions/prediction-chart/prediction-chart.component.ts
+++ src/app/components/charts/predictions/prediction-chart/prediction-chart.component.ts
@@ -44,6 +44,9 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
   isHighcharts = typeof Highcharts === 'object';
   chartConstructor: string = 'chart'; // optional string, defaults to 'chart'
   chartOptions: Highcharts.Options = {
+    legend:{
+      enabled:false
+    },
     
     series: [
       {
@@ -52,7 +55,8 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
       name: 'fixed points',
       events:{
         click: this.highchartSeriesClicked
-      }
+      },
+      
     },
     {
       data: [],
@@ -101,22 +105,6 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
 
 
 
-  todayAnnotation = {
-    type: 'line',
-    mode: 'vertical',
-    scaleID: 'days',
-    value: this.dateToStr(new Date()),
-    borderColor: 'orange',
-    borderWidth: 2,
-    label: {
-      enabled: true,
-      fontColor: 'orange',
-      content: ''
-    }
-  };
-
-
-
   displayChart: boolean = true;
 
 
@@ -152,272 +140,6 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
   DisplayMonth: boolean = true
 
 
-  public lineChartData: ChartDataSets[] = [
-    {
-      data: [],
-      label: 'Fixed points',
-      borderWidth: 1,
-      pointRadius: 4,
-      pointHitRadius: 6,
-      pointHoverBorderWidth: 8,
-      pointHoverBorderColor: 'rgb(255, 255, 255, 0.5)',
-      lineTension: 0,
-      spanGaps: false,
-    },
-
-    {
-      data: [],
-      label: 'History',
-      borderWidth: 6,
-      pointRadius: 1,
-      pointHitRadius: 6,
-      pointHoverBorderWidth: 8,
-      pointHoverBorderColor: 'rgb(255, 255, 255, 0.5)',
-      lineTension: 0,
-      spanGaps: false,
-    },
-    {
-      data: [],
-      label: 'Future',
-      borderWidth: 6,
-      pointRadius: 1,
-      pointHitRadius: 6,
-      pointHoverBorderWidth: 10,
-      pointHoverBorderColor: 'rgb(255, 255, 255, 0.5)',
-      lineTension: 0,
-      spanGaps: false,
-    },
-
-  ];
-
-
-  public lineChartLabels/*: Label[]*/ = [];
-  public lineChartOptions: (ChartOptions & { annotation?: any } & { pan?: any } & { zoom?: any }) = {
-    responsive: true,
-    tooltips: {
-      bodyFontSize: 16,
-      enabled: true,
-      mode: 'index',
-    },
-    maintainAspectRatio: false,
-    elements: {
-      line: {
-        tension: 0,
-
-      }
-    },
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-0',
-          position: 'left',
-          ticks: {
-            callback: (value, index, values) => {
-
-              let x = Number(value);
-              let i = 0;
-              while ((x / Math.pow(1000, i)) > 1000) {
-                i++
-              }
-              return ((x / Math.pow(1000, i))).toLocaleString() + 'k'.repeat(i);
-              //return value.toLocaleString(null, { maximumSignificantDigits: 3 });
-            },
-            display: true,
-          }
-          /*
-          gridLines: {
-            color: 'rgba(255,0,0,0.3)',
-          },
-          ticks: {
-            fontColor: 'red',
-          },
-          */
-
-        },
-      ],
-
-
-      xAxes: [
-        {
-          id: 'days',
-
-
-          type: "time",
-          distribution: 'linear',
-          time: {
-            minUnit: 'day',
-            unit: 'day',
-            round: 'day',
-            stepSize: 14,
-            displayFormats: {
-              month: 'MM.YYYY',
-              week: 'DD.MM.YYYY',
-              day: 'DD.MM.YYYY',
-              hour: 'DD.MM.YYYY',
-              minute: 'DD.MM.YYYY',
-            },
-          },
-
-          ticks: {
-            //sampleSize: 10,
-            autoSkipPadding: 14,
-            minRotation: 0,
-            maxRotation: 90,
-            //display: !this.compact,
-            /*
-            callback: (value, index, values) => {
-              if (('' + values[0]).substr(0, 4) === ('' + values[values.length - 1]).substr(0, 4)) {
-                return ('' + value).substring(5);
-              } else {
-                return value;
-              }
-            },
-            */
-
-          }
-        }
-      ]
-    },
-
-
-    annotation: {
-      annotations: [
-        /*
-        {
-          type: 'line',
-          mode: 'vertical',
-          scaleID: 'days',
-          value: this.dateToStr(new Date()),
-          borderColor: 'orange',
-          borderWidth: 2,
-          label: {
-            enabled: true,
-            fontColor: 'orange',
-            content: ''
-          }
-          
-        },
-        */
-      ]
-    },
-
-
-    // Container for pan options
-    pan: {
-      // Boolean to enable panning
-      enabled: true,
-
-      // Panning directions. Remove the appropriate direction to disable
-      // Eg. 'y' would only allow panning in the y direction
-      // A function that is called as the user is panning and returns the
-      // available directions can also be used:
-      //   mode: function({ chart }) {
-      //     return 'xy';
-      //   },
-      mode: 'x',
-
-      rangeMin: {
-        // Format of min pan range depends on scale type
-        x: null,
-        y: null
-      },
-      rangeMax: {
-        // Format of max pan range depends on scale type
-        x: null,
-        y: null
-      },
-
-
-      // On category scale, factor of pan velocity
-      speed: 2,
-
-      // Minimal pan distance required before actually applying pan
-      threshold: 0,
-
-      // Function called while the user is panning
-      //onPan: function ({ chart }) { console.log(`I'm panning!!!`); },
-      // Function called once panning is completed
-      //onPanComplete: function ({ chart }) { console.log(`I was panned!!!`); }
-    },
-
-    // Container for zoom options
-    zoom: {
-      // Boolean to enable zooming
-      enabled: true,
-
-      // Enable drag-to-zoom behavior
-      drag: false,
-
-      // Drag-to-zoom effect can be customized
-      // drag: {
-      // 	 borderColor: 'rgba(225,225,225,0.3)',
-      // 	 borderWidth: 5,
-      // 	 backgroundColor: 'rgb(225,225,225)',
-      // 	 animationDuration: 0
-      // },
-
-      // Zooming directions. Remove the appropriate direction to disable
-      // Eg. 'y' would only allow zooming in the y direction
-      // A function that is called as the user is zooming and returns the
-      // available directions can also be used:
-      //   mode: function({ chart }) {
-      //     return 'xy';
-      //   },
-      mode: 'x',
-
-      rangeMin: {
-        // Format of min zoom range depends on scale type
-        x: null,
-        y: null
-      },
-      rangeMax: {
-        // Format of max zoom range depends on scale type
-        x: null,
-        y: null
-      },
-
-
-      // Speed of zoom via mouse wheel
-      // (percentage of zoom on a wheel event)
-      speed: 0.1,
-
-      // Minimal zoom distance required before actually applying zoom
-      threshold: 0,
-
-      // On category scale, minimal zoom level before actually applying zoom
-      sensitivity: 1,
-
-      // Function called while the user is zooming
-      //onZoom: function ({ chart }) { console.log(`I'm zooming!!!`); },
-      // Function called once zooming is completed
-      //onZoomComplete: function ({ chart }) { console.log(`I was zoomed!!!`); }
-    }
-
-
-
-  };
-  public lineChartColors: Color[] = [
-    {
-      backgroundColor: 'rgba(192, 255, 0,1)',
-      borderColor: 'rgba(0, 255, 0)'
-
-    },
-    {
-      backgroundColor: 'rgba(255, 64, 192,0.3)',
-      borderColor: 'rgba(255, 64, 192)'
-
-    },
-    {
-      backgroundColor: 'rgba(32, 128, 255,0.3)',
-      borderColor: 'rgba(32, 128, 255)'
-    },
-
-  ];
-  public lineChartLegend = true;
-  public lineChartType: ChartType = 'line';
-  public lineChartPlugins = [pluginAnnotations, pluginZoom];
-
-
 
   constructor() {
     this.onDayClicked = new EventEmitter<Date>();
@@ -429,126 +151,11 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
 
   }
 
-  onChartClick(event: { event: MouseEvent, active: any[] }) {
-    console.log(event);
-    if (event.active.length >= 1) {
-      event.active.forEach(chartElement => {
-        console.log(chartElement);
-        //let clickedDate:Moment = moment(chartElement._xScale._labelItems[chartElement._index]);
-        let clickedDate: Moment = moment(this.lineChartLabels[chartElement._index]);
-        //let clickedDate:Moment = (this.lineChartLabels[chartElement._index]);
-
-        //if (clickedDate.getFullYear() < chartElement._xScale.min.substr(0, 4)) {
-        //  clickedDate.setFullYear(chartElement._xScale.min.substr(0, 4));
-        //}
-
-        console.log('=========================================================');
-        console.log('chartElement._xScale._minIndex : ', chartElement._xScale._minIndex);
-        console.log('clicked at index : ', chartElement._index);
-        console.log('this.lineChartLabels : ', this.lineChartLabels);
-        console.log('click at : ', clickedDate);
-        this.onDayClicked.emit(clickedDate.toDate());
-      })
-    }
-  }
-
-
-
-
-  clearAnnotations() {
-
-    this.lineChartOptions.annotation.annotations = [];
-    console.log(this.lineChartOptions.annotation.annotations);
-  }
-  lastDateTest = new Date();
-  addAnnotation() {
-
-    this.lineChartData[0].data.push(0);
-
-    this.lastDateTest.setDate(this.lastDateTest.getDate() + 1);
-
-
-
-    this.lineChartOptions.annotation.annotations.push({
-      type: 'line',
-      mode: 'vertical',
-      scaleID: 'days',
-      value: this.dateToStr(this.lastDateTest),
-      borderColor: 'red',
-      borderWidth: 4,
-      label: {
-        enabled: true,
-        fontColor: 'red',
-        content: ''
-      }
-    });
-
-
-    this.lineChartData[0].data.pop();
-
-    console.log(this.lineChartOptions.annotation.annotations);
-
-  }
-  chartRender() {
-    this.chartCanvas.chart.render({
-      duration: 800,
-      lazy: false,
-      easing: 'easeOutBounce'
-    });
-
-  }
-  chartUpdate() {
-    this.chartCanvas.chart.update({
-      duration: 800,
-      easing: 'easeOutBounce'
-    });
-    console.log(this.chartCanvas.chart);
-  }
-  chartDataUpdate() {
-    this.lineChartData = this.lineChartData.slice();
-
-  }
-
-
-  redrawChart() {
-    if (this.chart) {
-
-      this.chart?.update();
-      this.chart?.render();
-      this.lineChartData = this.lineChartData.slice();
-
-      try {
-        this.chart?.resetZoom();
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-
-  }
-
-
-
-
-
-
+  
 
   ngOnInit() {
     //console.log('this.compact = ', this.compact);
     this.loading$ = new BehaviorSubject<boolean>(null);
-
-    this.lineChartOptions.tooltips.enabled = !this.compact;
-    //this.lineChartOptions.scales.yAxes.forEach(y => y.ticks.display = !this.compact)
-    this.lineChartOptions.scales.xAxes.forEach(x => x.ticks.display = !this.compact)
-    this.lineChartLegend = !this.compact;
-
-
-
-
-    this.todayAnnotation.value = this.dateToStr(new Date());
-    this.lineChartOptions.annotation.annotations = [];
-    this.lineChartOptions.annotation.annotations.push(this.todayAnnotation);
-    console.log('this.todayAnnotation : ', this.todayAnnotation);
 
 
     this.loading$.next(true);
@@ -570,9 +177,6 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
 
         if (data) {
 
-          this.lineChartData[0].data = [];// fps
-          this.lineChartData[1].data = [];//history
-          this.lineChartData[2].data = [];//futur
 
           //highcharts:
 
@@ -580,21 +184,8 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
           let high_history = [];
           let high_futur = [];
           let categories = [];
-
           this.categoriesMap.clear();
-          /*
-          series: [{
-              data: [1, 2, 3],
-              type: 'line'
-            }]
-            */
-
-          let fps = []
-          let history = []
-          let futur = []
-
-          this.lineChartLabels = [];
-
+          
           let n = this.dateToStr(new Date());
           data.forEach(p => {
             let d = this.dateToStr(p.date);
@@ -613,42 +204,28 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
             }
 
             if (d < n) {
-              history.push(point);
-              futur.push(emptyPoint);
 
               high_history.push(point.y);
               high_futur.push(null);
 
             } else if (d > n) {
-              history.push(emptyPoint);
-              futur.push(point);
 
               high_history.push(null);
               high_futur.push(point.y);
             } else {
-              history.push(point);
-              futur.push(point);
 
               high_history.push(point.y);
               high_futur.push(point.y);
             }
 
             if (p.fixedPoint) {
-              fps.push({
-                x: m, 
-                y: p.fixedPoint.exact_value
-              });
+              
               high_fps.push(p.fixedPoint.exact_value);
             } else {
-              fps.push(emptyPoint);
               high_fps.push(null);
 
             }
 
-            
-
-
-            this.lineChartLabels.push(m);
             categories.push(m.format('DD-MM-YYYY'));
             this.categoriesMap.set(m.format('DD-MM-YYYY'), m.toDate())
           });
@@ -671,40 +248,18 @@ export class PredictionChartComponent implements OnInit, AfterViewInit {
           }
 
 
-
-          this.lineChartData[0].data = fps
-          this.lineChartData[1].data = history;
-          this.lineChartData[2].data = futur;
         }
 
         if (config) {
 
           this.delayOnUpdate = config.delayOnUpdate;
           if (config.disableControls) {
-            this.lineChartOptions.zoom.enabled = false;
-            this.lineChartOptions.pan.enabled = false;
+            
           }
 
 
           if (config.marks) {
-            this.lineChartOptions.annotation.annotations = [];
-            this.lineChartOptions.annotation.annotations.push(this.todayAnnotation);
-
-            config.marks.forEach(m => {
-              this.lineChartOptions.annotation.annotations.push({
-                type: 'line',
-                mode: 'vertical',
-                scaleID: 'days',
-                value: m,
-                borderColor: 'red',
-                borderWidth: 4,
-                label: {
-                  enabled: true,
-                  fontColor: 'yellow',
-                  content: ''
-                }
-              });
-            });
+            
           }
 
 
